75654f0bc10babafe7afd570116447163a712593
Wiley Conte <wileymckayconte@gmail.com>
2021-12-26 23:08:25 -0700

    Add crp_Candidate_id to migrations

--- db/migrations/20211227043616_AddLegiscanPeopleIdToPolitician.down.sql
+++ db/migrations/20211227043616_AddLegiscanPeopleIdToPolitician.down.sql
@@ -1,3 +1,6 @@
 -- Add down migration script here
 ALTER TABLE politician
 DROP COLUMN legiscan_people_id;
+
+ALTER TABLE politician
+DROP COLUMN crp_candidate_id;
--- db/migrations/20211227043616_AddLegiscanPeopleIdToPolitician.up.sql
+++ db/migrations/20211227043616_AddLegiscanPeopleIdToPolitician.up.sql
@@ -1,3 +1,6 @@
 -- Add up migration script here
 ALTER TABLE politician
 ADD COLUMN legiscan_people_id INT;
+
+ALTER TABLE politician
+ADD COLUMN crp_candidate_id INT;
