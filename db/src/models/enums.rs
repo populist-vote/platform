bb881bc41eaa679f3504697f519410ad3b762ba6
Wiley Conte <wileymckayconte@gmail.com>
2022-02-03 15:56:38 -0700

    Add state name impl

--- db/src/models/enums.rs
+++ db/src/models/enums.rs
@@ -16,6 +16,7 @@ use strum_macros::{Display, EnumIter, EnumString};
     Serialize,
     Deserialize,
 )]
+
 pub enum State {
     AL,
     AK,
@@ -73,6 +74,68 @@ pub enum State {
 impl Default for State {
     fn default() -> Self {
         State::CO
+    }
+}
+
+trait FullState {
+    fn full_state(&self) -> &str;
+}
+
+impl FullState for State {
+    fn full_state(&self) -> &str {
+        match self {
+            State::AL => "Alabama",
+            State::AK => "Alaska",
+            State::AZ => "Arizona",
+            State::AR => "Arkansas",
+            State::CA => "California",
+            State::CO => "Colorado",
+            State::CT => "Connecticut",
+            State::DC => "District of Columbia",
+            State::DE => "Delaware",
+            State::FL => "Florida",
+            State::GA => "Georgia",
+            State::HI => "Hawaii",
+            State::ID => "Idaho",
+            State::IL => "Illinois",
+            State::IN => "Indiana",
+            State::IA => "Iowa",
+            State::KS => "Kansas",
+            State::KY => "Kentucky",
+            State::LA => "Louisiana",
+            State::ME => "Maine",
+            State::MD => "Maryland",
+            State::MA => "Massachusetts",
+            State::MI => "Michigan",
+            State::MN => "Minnesota",
+            State::MS => "Mississippi",
+            State::MO => "Missouri",
+            State::MT => "Montana",
+            State::NE => "Nebraska",
+            State::NV => "Nevada",
+            State::NH => "New Hampshire",
+            State::NJ => "New Jersey",
+            State::NM => "New Mexico",
+            State::NY => "New York",
+            State::NC => "North Carolina",
+            State::ND => "North Dakota",
+            State::OH => "Ohio",
+            State::OK => "Oklahoma",
+            State::OR => "Oregon",
+            State::PA => "Pennsylvania",
+            State::RI => "Rhode Island",
+            State::SC => "South Carolina",
+            State::SD => "South Dakota",
+            State::TN => "Tennessee",
+            State::TX => "Texas",
+            State::UT => "Utah",
+            State::VT => "Vermont",
+            State::VA => "Virginia",
+            State::WA => "Washington",
+            State::WI => "Wisconsin",
+            State::WY => "Wyoming",
+            State::WV => "West Virginia",
+        }
     }
 }
 
