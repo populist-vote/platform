2c4846a69dd602160ebfb9d7f438aeb89401e1b4
Wiley Conte <wileymckayconte@gmail.com>
2022-03-09 10:42:17 -0700

    WIP

--- cli/src/bin/seed_2020_races.rs
+++ cli/src/bin/seed_2020_races.rs
@@ -1,21 +1,41 @@
 use std::error::Error;
 use std::process;
 
+use db::models::enums::{PoliticalParty, State};
 use db::CreateRaceInput;
 
 async fn seed_2020_races() -> Result<(), Box<dyn Error>> {
     db::init_pool().await.unwrap();
     let pool = db::pool().await;
 
-    // let us_senate_republican_primary = CreateRaceInput {
-    //     slug: "us-senate-republican-primary-2020-colorado",
-    //     title: "U.S. Senate Republican Primary",
-    //     office_position: "U.S. Senate",
-    //     office_id: None,
-    //     official_website: None,
-    //     race_type: db::models::enums::RaceType::Primary,
-    //     state: db::models::enums::State::CO,
-    // }
+    let office = sqlx::query!(
+        r#"
+        SELECT id FROM office WHERE slug = 'us-senate-co-1'
+    "#
+    )
+    .fetch_one(&pool.connection)
+    .await
+    .unwrap();
+
+    let us_senate_republican_primary = CreateRaceInput {
+        slug: Some("us-senate-republican-primary-2020-colorado".to_string()),
+        title: "U.S. Senate Republican Primary".to_string(),
+        office_position: "U.S. Senate".to_string(),
+        description: Some(
+            "Replublican primary election for the 2022 Colorado U.S. Senate Seat".to_string(),
+        ),
+        ballotpedia_link: Some(
+            "https://ballotpedia.org/United_States_Senate_election_in_Colorado,_2022".to_string(),
+        ),
+        early_voting_begins_date: None,
+        election_date: Some(chrono::NaiveDate::from_ymd(2022, 6, 28)),
+        party: Some(PoliticalParty::Republican),
+        office_id: office.id,
+        official_website: None,
+        race_type: db::models::enums::RaceType::Primary,
+        state: Some(State::CO),
+        election_id: None,
+    };
 
     Ok(())
 }
