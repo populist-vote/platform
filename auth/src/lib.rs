a32fab153c10c5e3ec71222557077f4d3d567d3d
Wiley Conte <wileymckayconte@gmail.com>
2022-06-19 01:58:58 -0600

    Ensure to strip plus signs form emails to suernames

--- auth/src/lib.rs
+++ auth/src/lib.rs
@@ -24,7 +24,7 @@ pub fn create_temporary_username(email: String) -> String {
     let mut rng = rand::thread_rng();
 
     let mut base = email.split('@').collect::<Vec<&str>>()[0].to_string();
-    base.retain(|c| !r#"(),"-;:'"#.contains(c));
+    base.retain(|c| !r#"+(),"-;:'"#.contains(c));
     let rnd_int: i32 = rng.gen();
     format!("{}{}", base, rnd_int)
 }
