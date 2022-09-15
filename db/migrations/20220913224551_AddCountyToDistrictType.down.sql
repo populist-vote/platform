d823d1b6f700a7ed878f4b6b6da7fb3ce2fbdf34
Wiley Conte <wileymckayconte@gmail.com>
2022-09-14 21:21:20 -0600

    Add back missing migration files

---
+++ db/migrations/20220913224551_AddCountyToDistrictType.down.sql
@@ -0,0 +1,4 @@
+CREATE TYPE district_type_new AS ENUM ('us_congressional', 'state_senate', 'state_house', 'school');
+ALTER TABLE office ALTER COLUMN district_type TYPE district_type_new USING district_type::text::district_type_new;
+DROP TYPE district_type;
+ALTER TYPE district_type_new RENAME TO district_type;
