ef5cc230c61b1918c882ded6a55db47c56ecf0a6
Wiley Conte <wileymckayconte@gmail.com>
2022-10-01 15:52:37 +0100

    Update process search query function

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -20,30 +20,28 @@ pub use models::user::*;
 pub use pool::*;
 
 /// This function takes in a string and returns a ts_query safe string for postgres
-/// For example "barack oba" becomes "barack | oba:*"
+/// For example "barack oba" becomes "barack%oba:*"
 fn process_search_query(query: String) -> String {
     if query.is_empty() {
         "".to_string()
     } else {
-        format!(
-            "{}{}",
-            query
-                .split_whitespace()
-                .map(|s| s.to_string())
-                .collect::<Vec<String>>()
-                .join(" | "),
-            ":*"
-        )
+        let mut formatted = format!("%{}", query)
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()
+            .join("%");
+        formatted.push_str(":*");
+        formatted
     }
 }
 
 #[test]
 fn test_process_search_query() {
     assert_eq!(process_search_query("".to_string()), "");
-    assert_eq!(process_search_query("foo".to_string()), "foo:*");
-    assert_eq!(process_search_query("foo bar".to_string()), "foo | bar:*");
+    assert_eq!(process_search_query("foo".to_string()), "%foo:*");
+    assert_eq!(process_search_query("foo bar".to_string()), "%foo%bar:*");
     assert_eq!(
         process_search_query("foo bar baz".to_string()),
-        "foo | bar | baz:*"
+        "%foo%bar%baz:*"
     );
 }
--- db/src/models/politician.rs
+++ db/src/models/politician.rs
@@ -434,7 +434,7 @@ impl Politician {
                         date_of_birth,
                         office_id,
                         thumbnail_image_url,
-                       official_website_url,
+                        official_website_url,
                         campaign_website_url,
                         facebook_url,
                         twitter_url,
