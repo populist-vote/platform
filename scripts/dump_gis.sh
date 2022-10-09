5b2d788feea89242e7fb7c987838d306a2298f0d
Carlos Perez <perez@doorstep.com>
2022-10-09 13:38:30 -0600

    Create a schema called p6t_state_mn

---
+++ scripts/dump_gis.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# pg_dump -Fc --no-acl --no-owner --format plain -h localhost -p 5433 -U cperez --table public.school_district_boundaries mngis1 > mngis1-schools-20221004.sql
+# sed -i.orig 's/public.geometry/heroku_ext.geometry/g' mngis1-schools-20221004.sql
+pg_dump -Fc --no-acl --no-owner --format plain --clean -h localhost -p 5433 -U cperez mngis1 > mngis1-all-20221009.sql
+# Database mngis1 is on the public schema. Instead, put our custom tables on schema p6t_state_mn.
+sed -i.orig -E \
+    -e '/(geom |spatial_ref_sys)/! s/ public\./ p6t_state_mn\./g' \
+    -e 's/DROP EXTENSION postgis;/-- DROP EXTENSION postgis;/g' \
+    mngis1-all-20221009.sql
+# Then copy in:
+# psql populist-platform-dev < mngis1-all-20221009.sql
