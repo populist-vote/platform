c9e21752ae4fa1f26c14d54c0b531cd724a3387f
Carlos Perez <perez@doorstep.com>
2022-10-10 07:59:46 -0600

    Use variables in dump script

--- scripts/dump_gis.sh
+++ scripts/dump_gis.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
-# pg_dump -Fc --no-acl --no-owner --format plain -h localhost -p 5433 -U cperez --table public.school_district_boundaries mngis1 > mngis1-schools-20221004.sql
-# sed -i.orig 's/public.geometry/heroku_ext.geometry/g' mngis1-schools-20221004.sql
-pg_dump -Fc --no-acl --no-owner --format plain --clean -h localhost -p 5433 -U cperez mngis1 > mngis1-all-20221009.sql
-# Database mngis1 is on the public schema. Instead, put our custom tables on schema p6t_state_mn.
+# On localhost, there is a database where shapefiles have been imported using shp2pgsql.
+# This exports the all the GIS tables from localhost and converts them into a format suitable for import into Heroku.
+DB=mngis1
+NOW=`date +"%Y%m%d"`
+SQL=$DB-all-$NOW.sql
+pg_dump -Fc --no-acl --no-owner --format plain --clean -h localhost -p 5433 -U cperez mngis1 > $SQL
+# Modify
+NEW_SCHEMA=p6t_state_mn
+# Database mngis1 is on the public schema. Instead, put our custom tables on a different schema.
 sed -i.orig -E \
-    -e '/(geom |spatial_ref_sys)/! s/ public\./ p6t_state_mn\./g' \
-    -e "s/(nextval|setval)\('public./\1('p6t_state_mn./g" \
+    -e "/(geom |spatial_ref_sys)/! s/ public\./ $NEW_SCHEMA\./g" \
+    -e "s/(nextval|setval)\('public./\1('$NEW_SCHEMA./g" \
     -e 's/DROP EXTENSION postgis;/-- DROP EXTENSION postgis;/g' \
-    mngis1-all-20221009.sql
+    $SQL
+echo Wrote $SQL
 # Then copy in:
 # psql populist-platform-dev < mngis1-all-20221009.sql
