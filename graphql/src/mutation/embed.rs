78af67ab4672e4dc3009c665eb5bb88f29acf370
Wiley Conte <wileymckayconte@gmail.com>
2023-04-10 11:43:02 -0600

    Add delete_embed mutation

--- graphql/src/mutation/embed.rs
+++ graphql/src/mutation/embed.rs
@@ -1,4 +1,4 @@
-use async_graphql::{Context, Object, Result};
+use async_graphql::{Context, Object, Result, SimpleObject};
 use auth::Claims;
 use db::{Embed, UpsertEmbedInput};
 use jsonwebtoken::TokenData;
@@ -7,6 +7,12 @@ use crate::{context::ApiContext, is_admin, types::EmbedResult};
 
 #[derive(Default)]
 pub struct EmbedMutation;
+
+#[derive(SimpleObject)]
+#[graphql(visible = "is_admin")]
+struct DeleteEmbedResult {
+    id: String,
+}
 
 #[Object]
 impl EmbedMutation {
@@ -45,5 +51,25 @@ impl EmbedMutation {
 
         let upserted_record = Embed::upsert(&db_pool, &input, &updated_by).await?;
         Ok(EmbedResult::from(upserted_record))
+    }
+
+    #[graphql(visible = "is_admin")]
+    async fn delete_embed(&self, ctx: &Context<'_>, id: uuid::Uuid) -> Result<DeleteEmbedResult> {
+        let db_pool = ctx.data::<ApiContext>()?.pool.clone();
+        let user_org_id = ctx
+            .data::<Option<TokenData<Claims>>>()
+            .unwrap()
+            .as_ref()
+            .unwrap()
+            .claims
+            .organization_id;
+
+        let existing_embed_org_id = Embed::find_by_id(&db_pool, id).await?.organization_id;
+        if existing_embed_org_id != user_org_id.unwrap() {
+            return Err("Unauthorized".into());
+        }
+
+        Embed::delete(&db_pool, id).await?;
+        Ok(DeleteEmbedResult { id: id.to_string() })
     }
 }
