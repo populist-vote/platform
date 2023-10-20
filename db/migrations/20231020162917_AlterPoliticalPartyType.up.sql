47516deaede3933fce76d5c4090cbc007ce7720e
Wiley Conte <wileymckayconte@gmail.com>
2023-10-20 11:28:02 -0600

    Alter party type

---
+++ db/migrations/20231020162917_AlterPoliticalPartyType.up.sql
@@ -0,0 +1,7 @@
+-- Add up migration script here
+ALTER TYPE political_party RENAME TO political_party_old;
+CREATE TYPE political_party AS ENUM ('democratic','republican','libertarian','green','constitution','unknown','independent','freedom','unity','approval_voting','unaffiliated','democratic_farmer_labor','grassroots_legalize_cannabis','legal_marijuana_now','socialist_workers','colorado_center','american_constitution');
+ALTER TABLE politician ALTER COLUMN party TYPE political_party USING party::text::political_party;
+ALTER TABLE race ALTER COLUMN party TYPE political_party USING party::text::political_party;
+ALTER TABLE user_profile ALTER COLUMN party TYPE political_party USING party::text::political_party;
+DROP TYPE political_party_old;
