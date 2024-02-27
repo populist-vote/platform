743d384d7008ebfde4165316f3df0dcb3f1527b0
Wiley Conte <wileymckayconte@gmail.com>
2024-02-26 23:43:05 -0700

    Add missing territories

---
+++ db/migrations/20240227064023_AddToStates.up.sql
@@ -0,0 +1,6 @@
+-- Add up migration script here
+ALTER TYPE state ADD VALUE 'AS';
+ALTER TYPE state ADD VALUE 'FM';
+ALTER TYPE state ADD VALUE 'MH';
+ALTER TYPE state ADD VALUE 'MP';
+ALTER TYPE state ADD VALUE 'PW';
