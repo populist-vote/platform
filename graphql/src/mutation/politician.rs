316198114de9462dfe7f3b7beccbf73d176a29c3
Wiley Conte <wileymckayconte@gmail.com>
2024-04-24 11:02:15 -0600

    Attempt cache clear

--- graphql/src/mutation/politician.rs
+++ graphql/src/mutation/politician.rs
@@ -256,11 +256,9 @@ impl PoliticianMutation {
 
         match result {
             Ok(politician) => {
-                tracing::warn!("politician.assets = {:?}", politician.assets);
                 DataLoaders::new(db_pool)
                     .politician_loader
-                    .feed_one(PoliticianSlug(slug), politician)
-                    .await;
+                    .clear::<PoliticianSlug>();
                 Ok(url)
             }
             Err(err) => {
