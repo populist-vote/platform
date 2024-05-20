fbaac99eb63dbb30c0c0932f80e0c0d8616b4562
Wiley Conte <wileymckayconte@gmail.com>
2024-05-20 10:55:00 -0600

    Add candidate guide to mutation

--- graphql/src/mutation/mutation.rs
+++ graphql/src/mutation/mutation.rs
@@ -1,9 +1,10 @@
 use super::{
     argument::ArgumentMutation, auth::AuthMutation, ballot_measure::BallotMeasureMutation,
-    bill::BillMutation, election::ElectionMutation, embed::EmbedMutation,
-    issue_tag::IssueTagMutation, office::OfficeMutation, organization::OrganizationMutation,
-    politician::PoliticianMutation, poll::PollMutation, question::QuestionMutation,
-    race::RaceMutation, user::UserMutation, voting_guide::VotingGuideMutation,
+    bill::BillMutation, candidate_guide::CandidateGuideMutation, election::ElectionMutation,
+    embed::EmbedMutation, issue_tag::IssueTagMutation, office::OfficeMutation,
+    organization::OrganizationMutation, politician::PoliticianMutation, poll::PollMutation,
+    question::QuestionMutation, race::RaceMutation, user::UserMutation,
+    voting_guide::VotingGuideMutation,
 };
 use async_graphql::MergedObject;
 #[derive(MergedObject, Default)]
@@ -13,6 +14,7 @@ pub struct Mutation(
     OrganizationMutation,
     BillMutation,
     BallotMeasureMutation,
+    CandidateGuideMutation,
     ElectionMutation,
     EmbedMutation,
     IssueTagMutation,
