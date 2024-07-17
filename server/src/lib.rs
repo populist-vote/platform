d6416f8fb3401c1f875e4e074f810686960503b9
Wiley Conte <wileymckayconte@gmail.com>
2024-07-17 13:39:59 -0600

    Wip on self signed certs

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,9 +1,16 @@
 use async_graphql::extensions::ApolloTracing;
-use axum::routing::get;
+use axum::{
+    extract::Host,
+    handler::HandlerWithoutStateExt,
+    http::{StatusCode, Uri},
+    response::Redirect,
+    routing::get,
+    BoxError,
+};
+use axum_server::tls_rustls::RustlsConfig;
 use dotenv::dotenv;
 use graphql::{context::ApiContext, new_schema};
-use std::net::SocketAddr;
-use tokio::net::TcpListener;
+use std::{env, net::SocketAddr, path::PathBuf};
 use tower_cookies::CookieManagerLayer;
 use tower_http::cors::CorsLayer;
 use tracing::info;
@@ -15,6 +22,13 @@ pub use cron::init_job_schedule;
 pub use jobs::*;
 mod handlers;
 pub use handlers::{graphql_handler, graphql_playground};
+
+#[allow(dead_code)]
+#[derive(Clone, Copy)]
+struct Ports {
+    http: u16,
+    https: u16,
+}
 
 pub async fn run() {
     dotenv().ok();
@@ -47,12 +61,65 @@ pub async fn run() {
         .layer(CorsLayer::very_permissive())
         .layer(CookieManagerLayer::new());
 
-    let port = std::env::var("PORT").unwrap_or_else(|_| "1234".to_string());
+    let env_port = env::var("PORT").unwrap_or_else(|_| "1234".to_string());
+    let port = env_port.parse::<u16>().unwrap_or_else(|_| 1234);
+
+    let ports = Ports {
+        http: port,
+        https: 1235,
+    };
+    tokio::spawn(redirect_http_to_https(ports));
+
+    // configure certificate and private key used by https
+    let config = RustlsConfig::from_pem_file(
+        PathBuf::from("certs/cert.pem"),
+        PathBuf::from("certs/key.pem"),
+    )
+    .await
+    .unwrap();
+
     info!("GraphQL Playground live at http://localhost:{}", &port);
-    let address = format!("0.0.0.0:{}", port);
+    let addr = SocketAddr::from(([127, 0, 0, 1], ports.https));
+
+    axum_server::bind_rustls(addr, config)
+        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+        .await
+        .unwrap()
+}
+
+#[allow(dead_code)]
+async fn redirect_http_to_https(ports: Ports) {
+    fn make_https(host: String, uri: Uri, ports: Ports) -> Result<Uri, BoxError> {
+        let mut parts = uri.into_parts();
+
+        parts.scheme = Some(axum::http::uri::Scheme::HTTPS);
+
+        if parts.path_and_query.is_none() {
+            parts.path_and_query = Some("/".parse().unwrap());
+        }
+
+        let https_host = host.replace(&ports.http.to_string(), &ports.https.to_string());
+        parts.authority = Some(https_host.parse()?);
+
+        Ok(Uri::from_parts(parts)?)
+    }
+
+    let redirect = move |Host(host): Host, uri: Uri| async move {
+        match make_https(host, uri, ports) {
+            Ok(uri) => Ok(Redirect::permanent(&uri.to_string())),
+            Err(error) => {
+                tracing::warn!(%error, "failed to convert URI to HTTPS");
+                Err(StatusCode::BAD_REQUEST)
+            }
+        }
+    };
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], ports.http));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(
-        TcpListener::bind(address).await.unwrap(),
-        app.into_make_service_with_connect_info::<SocketAddr>(),
+        listener,
+        redirect.into_make_service_with_connect_info::<SocketAddr>(),
     )
     .await
     .unwrap();
