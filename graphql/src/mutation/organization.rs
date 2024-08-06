cdf620c00cdbc2ffa73d64952ab87dede0164886
Wiley Conte <wileymckayconte@gmail.com>
2024-08-06 11:53:36 -0600

    Add organization thumbnail upload method

--- graphql/src/mutation/organization.rs
+++ graphql/src/mutation/organization.rs
@@ -1,15 +1,17 @@
 use crate::{
     context::ApiContext,
-    guard::StaffOnly,
+    guard::{OrganizationGuard, StaffOnly},
     is_admin,
     types::{Error, OrganizationResult},
+    upload_to_s3, File,
 };
 use async_graphql::*;
 use db::{
-    CreateOrConnectIssueTagInput, IssueTag, IssueTagIdentifier, Organization,
+    CreateOrConnectIssueTagInput, IssueTag, IssueTagIdentifier, Organization, OrganizationRoleType,
     UpdateOrganizationInput,
 };
 use sqlx::{Pool, Postgres};
+use std::io::Read;
 use std::str::FromStr;
 #[derive(Default)]
 pub struct OrganizationMutation;
@@ -63,7 +65,10 @@ pub async fn handle_nested_issue_tags(
 
 #[Object]
 impl OrganizationMutation {
-    #[graphql(guard = "StaffOnly", visible = "is_admin")]
+    #[graphql(
+        guard = "OrganizationGuard::new(&input.id.into(), &OrganizationRoleType::ReadOnly)",
+        visible = "is_admin"
+    )]
     async fn update_organization(
         &self,
         ctx: &Context<'_>,
@@ -77,6 +82,59 @@ impl OrganizationMutation {
         }
 
         Ok(OrganizationResult::from(new_record))
+    }
+
+    #[graphql(
+        guard = "OrganizationGuard::new(&id.clone().into(), &OrganizationRoleType::ReadOnly)",
+        visible = "is_admin"
+    )]
+
+    async fn upload_organization_thumbnail(
+        &self,
+        ctx: &Context<'_>,
+        id: ID,
+        file: Upload,
+    ) -> Result<String> {
+        let db_pool = ctx.data::<ApiContext>()?.pool.clone();
+
+        let upload = file.value(ctx).unwrap();
+        let mut content = Vec::new();
+        let slug =
+            db::Organization::find_by_id(&db_pool, uuid::Uuid::parse_str(id.as_str()).unwrap())
+                .await?
+                .slug;
+        let filename = format!("{}-400", slug);
+        let mimetype = upload.content_type.clone();
+
+        upload.into_read().read_to_end(&mut content).unwrap();
+        let file_info = File {
+            id: ID::from(uuid::Uuid::new_v4()),
+            filename,
+            content,
+            mimetype,
+        };
+        let url = upload_to_s3(file_info, "web-assets/organization-thumbnails".to_string()).await?;
+        // Append last modified date because s3 path will remain the same and we want browser to cache, but refresh the image
+        let url = format!("{}{}{}", url, "?lastmod=", chrono::Utc::now().timestamp());
+
+        let result = sqlx::query_as!(
+            Organization,
+            r#"
+            UPDATE organization
+            SET assets = jsonb_set(jsonb_set(assets, '{thumbnailImage160}', $1::jsonb, true), '{thumbnailImage400}', $1::jsonb, true)
+            WHERE id = $2
+            RETURNING *
+            "#,
+            serde_json::json!(url),
+            uuid::Uuid::parse_str(&id).unwrap()
+        )
+        .fetch_one(&db_pool)
+        .await?;
+
+        Ok(result.assets["thumbnailImage400"]
+            .as_str()
+            .unwrap()
+            .to_string())
     }
 
     #[graphql(guard = "StaffOnly", visible = "is_admin")]
