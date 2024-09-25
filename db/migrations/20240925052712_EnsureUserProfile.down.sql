fe8756c001ee343cdd282e95775c936a187d585d
Wiley Conte <wileymckayconte@gmail.com>
2024-09-24 23:50:02 -0600

    Fix migration

--- db/migrations/20240925052712_EnsureUserProfile.down.sql
+++ db/migrations/20240925052712_EnsureUserProfile.down.sql
@@ -8,3 +8,7 @@ DROP TRIGGER IF EXISTS create_user_profile ON populist_user;
 
 -- Drop the function
 DROP FUNCTION IF EXISTS ensure_user_profile_exists;
+
+-- Drop the unique constraint
+ALTER TABLE user_profile
+DROP CONSTRAINT IF EXISTS unique_user_id;
--- db/migrations/20240925052712_EnsureUserProfile.up.sql
+++ db/migrations/20240925052712_EnsureUserProfile.up.sql
@@ -1,4 +1,6 @@
 -- Add up migration script here
+
+
 CREATE OR REPLACE FUNCTION ensure_user_profile_exists()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -11,14 +13,14 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- Step 2: Create a trigger that calls the function whenever a 
+-- Create a trigger that calls the function whenever a 
 -- populist_user is inserted
 CREATE TRIGGER create_user_profile
 AFTER INSERT ON populist_user
 FOR EACH ROW
 EXECUTE FUNCTION ensure_user_profile_exists();
 
--- Step 3: Optional - Add a foreign key constraint to ensure that 
+-- Add a foreign key constraint to ensure that 
 -- every user_profile  has a valid populist_user
 ALTER TABLE user_profile
 ADD CONSTRAINT fk_user_profile_user
@@ -26,3 +28,8 @@ FOREIGN KEY (user_id)
 REFERENCES populist_user (id)
 ON UPDATE CASCADE
 ON DELETE CASCADE;
+
+-- Add a unique constraint to ensure that each user_id in user_profile is unique
+ALTER TABLE user_profile
+ADD CONSTRAINT unique_user_id
+UNIQUE (user_id);
