29fc36417bd8c4316fd301895b9f745bbb00eca5
Wiley Conte <wileymckayconte@gmail.com>
2024-12-11 23:08:19 -0700

    Add migrations to ensure cascade deletes on convo

---
+++ db/migrations/20241212052420_AddCascadeToConvo.down.sql
@@ -0,0 +1,12 @@
+-- Add down migration script here
+ALTER TABLE statement
+DROP CONSTRAINT IF EXISTS statement_conversation_id_fkey,
+ADD CONSTRAINT statement_conversation_id_fkey
+FOREIGN KEY (conversation_id)
+REFERENCES conversation (id);
+
+ALTER TABLE statement_vote
+DROP CONSTRAINT IF EXISTS statement_vote_statement_id_fkey,
+ADD CONSTRAINT statement_vote_statement_id_fkey
+FOREIGN KEY (statement_id)
+REFERENCES statement (id);
